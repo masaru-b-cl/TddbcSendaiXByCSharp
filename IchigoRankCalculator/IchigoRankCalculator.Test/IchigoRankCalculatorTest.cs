fd9ca3ecfdfd45c756fa8f23e5be1cae4999de88
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2021-04-22 06:08:49 +0900

    rename first test method

--- IchigoRankCalculator/IchigoRankCalculator.Test/IchigoRankCalculatorTest.cs
+++ IchigoRankCalculator/IchigoRankCalculator.Test/IchigoRankCalculatorTest.cs
@@ -6,7 +6,7 @@ namespace IchigoRankCalculator.Test
     public class いちごの作成
     {
         [Fact]
-        public void Test1()
+        public void 品種にあまおう_サイズにSを与えていちごを作成する()
         {
 
         }
