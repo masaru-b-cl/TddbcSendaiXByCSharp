965b1814186e6b7819e0d3c6f472b361e5a268ce
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2021-04-23 22:30:05 +0900

    implement ichigo fake it

--- IchigoRankCalculator/IchigoRankCalculator/Ichigo.cs
+++ IchigoRankCalculator/IchigoRankCalculator/Ichigo.cs
@@ -1,9 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace IchigoRankCalculator
 {
     public class Ichigo
     {
+        public Ichigo(string breed, string size)
+        {
+        }
+
+        public string Breed => "あまおう";
+
+        public string Size => "S";
     }
 }
